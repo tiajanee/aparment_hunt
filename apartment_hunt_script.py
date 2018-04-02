d8ce6ff4fe2d4e3a5323ddb82da88ba7d2bb9845
Tia King <tiajaneeking@gmail.com>
2018-04-01 18:34:23 -0700

    add csv creation function and listing_info function, made code modular

--- apartment_hunt_script.py
+++ apartment_hunt_script.py
@@ -12,99 +12,108 @@ import os
 APARTMENT_LINKS = [
 "https://www.apartments.com/san-leandro-racquet-club-san-leandro-ca/64kvwjs/",
 "https://www.apartments.com/amber-court-apartment-homes-fremont-ca/x4hhv4n/",
-"https://www.apartments.com/the-heights-san-leandro-ca/s82z2b6/"
+"https://www.apartments.com/the-heights-san-leandro-ca/s82z2b6/",
+"https://www.apartments.com/creekwood-hayward-ca/per3r3t/"
 ]
 
-DIR = '/Users/tiaking/Desktop'
+DIR = '/Users/tiaking/Desktop/apartment_hunt/apartment_listings.csv'
 
-def getting_amenities():
+def main():
+	for apartment in APARTMENT_LINKS:
+		listing_info = get_listing_attributes(apartment)
+		extras = getting_amenities(apartment)
+		# create_apartment_dataset(listing_info, extras)
+
+
+def getting_amenities(apartment):
 
 
 #getting the amenities, checking for gym, pool/sauna/spa
 
-	for i in range(len(APARTMENT_LINKS)):
-		page = urlopen(APARTMENT_LINKS[i]).read()
-		soup = BeautifulSoup(page, 'html')
-		# print(soup)
-		# soup.prettify()
-		# pprint.pprint(soup)
-		import json
-		results = soup.find_all('div', {"class" : "col-33"})
-		# print(results[0])
-		
-		ameneties = [result.text for result in soup('div', {"class" : "col-33"})]
-		# print(ameneties)
-		am_string = ""
-		for i in range(len(ameneties)):
-			am_string += ameneties[i]
-		" ".join(am_string.split())
-		# print(am_string)
+	page = urlopen(apartment).read()
+	soup = BeautifulSoup(page, 'html')
+	
+	ameneties = ([result.text for result in soup('div', {"class" : "col-33"})][0].strip() + "\n"
+		+ [result.text for result in soup('div', {"class" : "col-33"})][1].strip())
+	# print(ameneties)
 
 
-		check_gym_list = ["Fitness", "Gym", "Cardio"]
+	check_gym_list = ["Fitness", "Gym", "Cardio"]
 
-		has_gym = False
+	has_gym = False
 
-		for gym in check_gym_list:
-			if gym in am_string:
-				has_gym = True
+	for gym in check_gym_list:
+		if gym in ameneties:
+			print(gym)
+			has_gym = True
 
-		check_pool_list = ["Pool", "Sauna", "Spa", "Hot Tub"]
+	check_pool_list = ["Pool", "Sauna", "Spa", "Hot Tub"]
 
-		has_pool =  False
+	has_pool =  False
 
-		for pool in check_pool_list:
-			if pool in am_string:
-				has_pool = True
+	for pool in check_pool_list:
+		if pool in ameneties:
+			print(pool)
+			has_pool = True
 
 	# print(has_gym)
 	# print(has_pool)
+	return ameneties
 
 
 
+def get_listing_attributes(apartment):
+	page = urlopen(apartment).read()
+	soup = BeautifulSoup(page, 'html')
+	# pprint.pprint(soup)
 
-def get_listing_attributes():
-	for i in range(len(APARTMENT_LINKS)):
-		page = urlopen(APARTMENT_LINKS[i]).read()
-		soup = BeautifulSoup(page, 'html')
-		pprint.pprint(soup)
-
-		lease_length = [lease.text for lease in soup('li', {'class':"leaseLength"})]
-		lease_string = ""
-		for i in range(len(lease_length)):
-			lease_string += lease_length[i]
-
-		property_name = [name.text for name in soup('div', {'class':'propertyName'})][0]
-		for character in property_name.lower():
-			# print(character)
-			# print(property_name.index(character))
-			# no_check_list = [0,1, len(property_name), len(property_name)-1]
-			# if property_name.index(character) not in no_check_list:
-			# 	separator_check_one = property_name.index(character)-1
-			# 	separator_check_two = property_name.index(character)+1
-			if character not in 'abcdefghijklmnopqrstuvwxyz':
-				new_str = property_name.replace(character, "")
-
-		ratings = soup.find('div', class_='rating')
-		title = ratings.get("title")
-		# print(property_name)
-		# print(new_str)
-		# print(title)
+	lease_length = [lease.text for lease in soup('li', {'class':"leaseLength"})][0].strip()
 	
+	property_name = [name.text for name in soup('div', {'class':'propertyName'})][0].strip()
+
+	rating = soup.find('div', class_='rating').get("title")
+	address = [location.text for location in soup('span', {'itemprop': 'streetAddress'})][0]
+	city = [city.text for city in soup('span', {'itemprop': 'addressLocality'})][0] 
+	phone_number = [number.text for number in soup('span', {'class': 'phoneNumber'})][0].strip()
+
+	deposit = [money.text for money in soup('td', {'class': 'deposit'})][0]
+
+	size = [feet.text for feet in soup('td', {'class': 'sqft'})][0]
+
+	rent_range = [rent.text for rent in soup.find_all('div', {'class': 'rentRollupContainer'})][0].split()[-1].strip()
+	application_fee = [fee.text for fee in soup('div', {'class':'oneTimeFees'})][0].split()[3]
 	
+	listing = [property_name, 
+	address, city, rating, phone_number, lease_length, deposit, size, rent_range, application_fee]
+
+	return listing
+
+def create_apartment_dataset(listing, ameneties):
+	file_path = DIR
+
+	if not os.path.exists(os.path.dirname(file_path)):
+			try:
+				os.makedirs(os.path.dirname(file_path))
+			except OSError as exc: # Guard against race condition
+				if exc.errno != errno.EEXIST:
+					raise
+	
+	with open(file_path, 'a') as csvfile:
+		filewriter = csv.writer(csvfile, delimiter =",", quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		if os.path.getsize(file_path) == 0:
+			filewriter.writerow(['name','address', 'city', 'rating', 'phone #', 'lease length', 'deposit',
+				'size', 'rent $', "app fee", "ameneties"])
+
+		print(listing)
+		print(ameneties)
+		listing.append(ameneties)
+		print(listing)
+		filewriter.writerow(listing)
 
 
 
-
-
-
-get_listing_attributes()
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
 
 
 
